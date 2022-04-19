48741598f9726e6ea2b8d3d5bb7347ef8ea905f7
xiangfei001 <103406604+xiangfei001@users.noreply.github.com>
2022-04-19 11:02:38 +0800

    Add files via upload

---
+++ 新建文件夹/function myFunction(){.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    //Cro xiangfei
+    //1. Data can be stored 2 Communication with server 3 Modify HTML documen
+    let x = document.getElementById("userInput1").value;
+    let y = document.getElementById("userInput2").value;
+    let z =parseInt(x)+parseInt(y);
+    document.getElementById("demo").innerHTML = z;
